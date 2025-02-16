260c60cea7291cd7594f5a018903b3d74571eba5
Ney Barbosa <esmererneyolivera@gmail.com>
2025-02-16 13:47:48 -0500

    feat: Implementacion de SEO y query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
+import "./index.css";
+
+//Instancia de queryCliente
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
