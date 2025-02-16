6630c5e6a6b28f2acd4b7e6ccfd8a73335e47538
Ney Barbosa <esmererneyolivera@gmail.com>
2025-02-16 13:18:07 -0500

    fix: mejora en clase

--- src/components/PokemonTable.tsx
+++ src/components/PokemonTable.tsx
@@ -29,7 +29,7 @@ const PokemonTable = () => {
           ))}
         </tbody>
       </table>
-      <div className="flex justify.between mt-4">
+      <div className="flex justify-between mt-4">
         <button onClick={() => setPage((p) => Math.max(p - 1, 1))} className="bg-blue-500 text-white px-4 py-2 rounded">
           Anterior
         </button>
