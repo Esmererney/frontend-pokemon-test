b035cf4b10b14ce17a70e5c03c90ec130803e5ae
Ney Barbosa <esmererneyolivera@gmail.com>
2025-02-16 13:18:47 -0500

    feat: Implementacion de SEO y componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import PokemonTable from "./components/PokemonTable";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HelmetProvider>
+        <Helmet>
+          <title>Pokédex | Lista de Pokémon</title>
+           <meta name="description" content="Explora la lista de Pokémon con imágenes y búsqueda interactiva." />
+          <meta name="keywords" content="Pokémon, Pokédex, API, React, TypeScript" />
+          <meta property="og:title" content="Pokédex | Lista de Pokémon" />
+          <meta property="og:description" content="Explora y filtra tu Pokémon favorito en esta Pokédex interactiva." />
+        </Helmet>
+        <PokemonTable />
+      </HelmetProvider>
     </>
   )
 }
